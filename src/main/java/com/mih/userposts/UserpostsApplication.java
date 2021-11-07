3a396780da160329afa18550ea556aa4867ba3dc
mhavilah <mhavilah@gmail.com>
2021-11-07 22:31:34 +1100

    Added CORS Cross origin support

--- src/main/java/com/mih/userposts/UserpostsApplication.java
+++ src/main/java/com/mih/userposts/UserpostsApplication.java
@@ -2,6 +2,13 @@ package com.mih.userposts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static com.mih.userposts.api.UserPostEndpoint.USERS_AND_POSTS_ENDPOINT_URI;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.HEAD;
 
 @SpringBootApplication
 public class UserpostsApplication {
@@ -10,4 +17,27 @@ public class UserpostsApplication {
 		SpringApplication.run(UserpostsApplication.class, args);
 	}
 
+	/**
+	 * Cross-origin/CORS configuration.
+	 * This allows clients (eg, Angular) who have been served from a front-end web server (eg, NodeJS) running
+	 * from a different port to connect to these APIs, running on the Spring Boot Tomcat server on port 8080.
+	 * See:
+	 * https://spring.io/guides/gs/rest-service-cors/#global-cors-configuration
+	 *
+	 * Clients will issue 'preflight' queries via HTTP OPTION to check CORS constraints.
+	 *
+	 * @return WebMvcConfigurer
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api"+ USERS_AND_POSTS_ENDPOINT_URI)
+						.allowedOrigins("http://localhost:8080", "http://localhost:4200")
+						.allowedMethods(GET.name(), HEAD.name() )
+						.allowCredentials(true);
+			}
+		};
+	}
 }
--- src/main/java/com/mih/userposts/api/UserPostEndpoint.java
+++ src/main/java/com/mih/userposts/api/UserPostEndpoint.java
@@ -2,21 +2,25 @@ package com.mih.userposts.api;
 
 import com.mih.userposts.model.UserPost;
 import com.mih.userposts.service.UserPostServiceImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class UserPostEndpoint {
 
     private final UserPostServiceImpl userPostService;
 
+    public static final String USERS_AND_POSTS_ENDPOINT_URI = "/usersAndPosts";
+
     public UserPostEndpoint(UserPostServiceImpl userPostService) {
         this.userPostService = userPostService;
     }
 
-    @GetMapping("/usersAndPosts")
+    @GetMapping(USERS_AND_POSTS_ENDPOINT_URI)
     List<UserPost> all() {
 
         // TODO - check for pagination query params...
